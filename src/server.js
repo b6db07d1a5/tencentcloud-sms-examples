d517d4b8d37742bd9266bcfd0822684879104853
Ananass <sitthichai@tencent.co.th>
2019-07-25 18:07:44 +0700

    Add ngrok

--- src/server.js
+++ src/server.js
@@ -1,8 +1,27 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 3000
 
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+    limit: '2mb'
+  })
+)
+app.use(
+  bodyParser.json({
+    limit: '2mb'
+  })
+)
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/callback', (req, res) => {
+  console.log(JSON.stringify(req.body))
+
+  res.json(req.body)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
