83629761141dca82619af73390d17aaaede218f5
Ananass <sitthichai@tencent.co.th>
2019-07-26 12:00:28 +0700

    Add generate sig

--- src/server.js
+++ src/server.js
@@ -30,27 +30,34 @@ app.post('/send/single', async (req, res) => {
   const random = utils.random()
   const time = new Date().getTime()
 
-  const smsResult = await axios.post(
-    `${smsUrl}sdkappid=${process.env.APP_ID}&random=${random}`,
-    {
-      ext: '',
-      extend: '',
-      params: ['Verification code', '1234', '4'],
-      sig: 'ecab4881ee80ad3d76bb1da68387428ca752eb885e52621a3129dcf4d9bc4fd4',
-      sign: 'Tencent Cloud',
-      tel: {
-        mobile: '13788888888',
-        nationcode: '86'
-      },
-      time,
-      tpl_id: 19
-    }
-  )
+  const sig = utils.sig({
+    appkey: 'xxx',
+    random,
+    time,
+    mobile: 'xxx'
+  })
 
-  console.log(process.env.APP_ID)
+  // const smsResult = await axios.post(
+  //   `${smsUrl}sdkappid=${process.env.APP_ID}&random=${random}`,
+  //   {
+  //     ext: '',
+  //     extend: '',
+  //     params: ['Verification code', '1234', '4'],
+  //     sig: 'ecab4881ee80ad3d76bb1da68387428ca752eb885e52621a3129dcf4d9bc4fd4',
+  //     sign: 'Tencent Cloud',
+  //     tel: {
+  //       mobile: '13788888888',
+  //       nationcode: '86'
+  //     },
+  //     time,
+  //     tpl_id: 19
+  //   }
+  // )
+
+  // console.log(process.env.APP_ID)
 
   res.json({
-    gg: process.env.APP_ID
+    gg: sig
   })
 })
 
--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,13 @@
-export function sha256() {}
+import crypto from 'crypto'
+
+export function sig({ appkey, random, time, mobile }) {
+  const strSig = `appkey=${appkey}&random=${random}&time=${time}&mobile=${mobile}`
+
+  return crypto
+    .createHash('sha256')
+    .update(strSig)
+    .digest('hex')
+}
 
 export function random() {
   return Math.floor(Math.random() * 100)
