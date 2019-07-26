8a79172750075590bd64a66b4f82b1c070945e15
Ananass <sitthichai@tencent.co.th>
2019-07-26 12:14:21 +0700

    Add send single to mobile

--- src/server.js
+++ src/server.js
@@ -31,37 +31,33 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/send/single', async (req, res) => {
   const random = utils.random()
-  const time = new Date().getTime()
+  const time = Math.floor(new Date() / 1000)
 
   const sig = utils.sig({
     appKey,
     random,
     time,
-    mobile: 'xxx'
+    mobile: '957529362'
   })
 
-  // const smsResult = await axios.post(
-  //   `${smsUrl}sdkappid=${process.env.APP_ID}&random=${random}`,
-  //   {
-  //     ext: '',
-  //     extend: '',
-  //     params: ['Verification code', '1234', '4'],
-  //     sig: 'ecab4881ee80ad3d76bb1da68387428ca752eb885e52621a3129dcf4d9bc4fd4',
-  //     sign: 'Tencent Cloud',
-  //     tel: {
-  //       mobile: '13788888888',
-  //       nationcode: '86'
-  //     },
-  //     time,
-  //     tpl_id: 19
-  //   }
-  // )
+  const smsResult = await axios.post(
+    `${smsUrl}sdkappid=${appId}&random=${random}`,
+    {
+      ext: '',
+      extend: '',
+      params: ['1234', '4'],
+      sig,
+      sign: 'Tencent Cloud',
+      tel: {
+        mobile: '957529362',
+        nationcode: '66'
+      },
+      time,
+      tpl_id: 381175
+    }
+  )
 
-  // console.log(process.env.APP_ID)
-
-  res.json({
-    gg: sig
-  })
+  res.json(smsResult.data)
 })
 
 app.post('/callback', (req, res) => {
